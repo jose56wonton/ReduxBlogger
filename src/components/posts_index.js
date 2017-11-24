279c38fe397d2816e0dae7e75e5094a8ebbeb6f2
Joshua Wootonn <jose56wonton@gmail.com>
2017-11-23 21:02:53 -0600

    Finished the blogger without styling

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -11,9 +11,11 @@ class PostsIndex extends Component{
   renderPosts(){
     return _.map(this.props.posts, post =>{
       return(
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
+        <Link to={`/posts/${post.id}`}>
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        </Link>   
       )
     })
   }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,20 @@
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS,FETCH_POST,DELETE_POST} from '../actions';
 import _ from 'lodash';
 
 export default function(state = {},action){
   switch(action.type){
+    case DELETE_POST:
+      return _.omit(state, action.payload);
+
+    case FETCH_POST:
+
+      // ES5
+      // const newState ={...state};
+      // newState[post.id] = post;
+      // return newState
+      // ES6
+      return { ...state, [action.payload.data.id]: action.payload.data };
+      break;
     case FETCH_POSTS :
       
       // This is done with lodash!!!
@@ -10,6 +22,9 @@ export default function(state = {},action){
       // and the object of that id being the value
       return _.mapKeys(action.payload.data, 'id');
       break;
+    
+
+   
     default: 
       return state;
   }
